b798480d63c66ff824a9e2083dc1b0503ca976b6
Govind Heda <111783844+govindheda070504@users.noreply.github.com>
2023-02-03 22:25:07 +0530

    Update 3Feb.py

--- Python/3Feb.py
+++ Python/3Feb.py
@@ -1,1 +1,29 @@
+#program to find position of all the letters present in the string 
+s1="abcagha"
+start=0
+str='a'
+x=s1.find(str,0)
+while(x1=-1)
+    print(x)
+    start=x+len(str)
+    x=s1.find(str,start)
+    
+    
+s1="i am learning python"
+l=s1.split()
+print(l)
 
+
+#program to output  the name in the form of A.kumar(ankit kumar)/P.K.Rajput(pushpendra kumar rajput)/amit(amit)
+short_name=" "
+name=input("enter name ")
+name= name.title()
+l=name.split()
+length=len(l)
+for i in range(length-1):
+  short_name=short_name+l[i][0]+"."
+  short_name+=l[length-1]
+  
+#program to find the wheather the string is anagram 
+
+ 
