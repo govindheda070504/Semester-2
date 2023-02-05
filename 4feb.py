44a4aa6c7350429963763d103e7d9b99765d436e
Govind Heda <111783844+govindheda070504@users.noreply.github.com>
2023-02-06 01:08:18 +0530

    Create 4feb.py

---
+++ 4feb.py
@@ -0,0 +1,23 @@
+#reversing of a string "hello how are you "
+st= input("enter a string ")
+l=st.split()
+result=""
+for word in l:
+    result=result+word[::-1]+""
+    print (result)
+    
+#printing the short form 
+st = input("enter a string ")
+sort_form = ''.join(sorted(st))
+reverse_form=st[::-1]
+
+#to print the frequency of every string 
+st= input("ENTER  A STRING" )
+st1=""
+for char in string:
+    if char not in st:
+        st1=st1+char
+        print(st1)
+ for ch i st1 
+          
+
